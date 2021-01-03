e8132d07cc49bd192d2b8f88d713270ae3d867e9
AICHOUN-C <cherif.aichoun@gmail.com>
2021-01-03 16:07:42 +0100

    feat: challenge done!

--- src/main/java/com/questspringrest/repository/BookRepository.java
+++ src/main/java/com/questspringrest/repository/BookRepository.java
@@ -1,5 +1,14 @@
 package com.questspringrest.repository;
 
-public interface BookRepository {
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.questspringrest.entity.Book;
+
+@Repository
+public interface BookRepository extends JpaRepository<Book, Long>{
+
+	List<Book> findByTitleContaining(String searchTerm);
 }
